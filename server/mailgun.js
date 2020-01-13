17244cde26d2e8e23cc5c4e69a1486d9df96c9ba
Jeaber <jeaber@gmail.com>
2020-01-13 09:09:15 -0600

    add dotenv

--- server/mailgun.js
+++ server/mailgun.js
@@ -1,6 +1,7 @@
 /**
  * Created by jeaber on 7/20/15.
  */
+const api_key = process.env.MAILGUN; // hide api keys in hidden file called .env
 
 var nodemailer = require('nodemailer');
 var mg = require('nodemailer-mailgun-transport');
@@ -8,14 +9,14 @@ var mg = require('nodemailer-mailgun-transport');
 // This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
 var auth = {
     auth: {
-        api_key: 'key-7acc68981a1e1a2a477284eedfd8e0b8',
+        api_key,
         domain: 'sandbox3f0214daaf924634927f6d309cbbadd7.mailgun.org'
     }
 }
 // This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
 auth2 = {
     auth: {
-        api_key: 'key-7acc68981a1e1a2a477284eedfd8e0b8',
+        api_key,
         domain: 'mg.frontierpropertiesmn.com'
     }
 }
